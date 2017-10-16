4805477f4d50ec0fc687cea2e71a9396bbedc426
Zack Robinson <zwrprofessional@gmail.com>
2017-10-16 00:04:19 -0400

    Added code for querying bonded bluetooth devices

--- app/src/main/java/com/zackeryrobinson/bluetooth/MainActivity.java
+++ app/src/main/java/com/zackeryrobinson/bluetooth/MainActivity.java
@@ -1,17 +1,57 @@
 package com.zackeryrobinson.bluetooth;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 
-//Changes Committed by Edel
+import java.util.Set;
+
 
 public class MainActivity extends AppCompatActivity {
+
+
+    //The system passes this constant back to you
+    // in your onActivityResult() implementation as the requestCode parameter.
+    private static final int REQUEST_ENABLE_BT = 1;
+    private static final String TAG = "MainActivityTag";
+    private BluetoothAdapter mBluetoothAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBluetoothAdapter == null) {
+            // Device does not support Bluetooth
         }
 
+        if (!mBluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+
+
+
+    }
+
+    private void getMyBondedDevices(Set<BluetoothDevice> pairedDevices) {
+        if (pairedDevices.size() > 0) {
+            // There are paired devices. Get the name and address of each paired device.
+            for (BluetoothDevice device : pairedDevices) {
+                String deviceName = device.getName();
+                String deviceHardwareAddress = device.getAddress(); // MAC address
+                Log.d(TAG, "getMyBondedDevices: DeviceName: " + deviceName + ", Device Hardware Address: " + deviceHardwareAddress);
+            }
+        }
+    }
+
+    public void getMyBondedDevices(View view) {
+        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+        getMyBondedDevices(pairedDevices);
+    }
 }
